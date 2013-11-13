d79c359c8257460a51a969856334cd7d083cc0b0
yank555-lu <yank555.lu@gmail.com>
2013-11-13 17:39:32 +0100

    Update 12 -> SEA Update 1 (sources)
    I9300XXUGMJA

--- kernel/sched.c
+++ kernel/sched.c
@@ -4297,6 +4297,7 @@ need_resched:
 		rq->curr = next;
 		++*switch_count;
 
+
 		context_switch(rq, prev, next); /* unlocks the rq */
 		/*
 		 * The context switch have flipped the stack from under us
